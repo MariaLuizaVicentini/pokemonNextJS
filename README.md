7e189b5dc34511382821efaca48bb26cb89c58bd
malu vicentinim <vicentinimalu1@gmail.com>
2026-01-18 13:02:20 -0300

    testando roteamento nextJS

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 ![Preview do Projeto](./imgnext.webp)
+
+
+------
+
+
+# CRIANDO O PROJETO
+
+```
+npx-create-next-app <nome>
+```
+
+
+-----
+
+# Rodando o projeto
+```
+npm run dev
+```
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
-       
+       <div className={styles.container}>
+        <h1>Hello Word Netx.js</h1>
+       </div>
   )
 }
