832dcff2d162ea62aa747667a22934bee2ad8d25
malu vicentinim <vicentinimalu1@gmail.com>
2026-01-18 14:33:08 -0300

    navegação entre páginas através do elemento Link

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from 'next/link'
 import styles from "@/styles/Home.module.css";
-
 
 export default function Home() {
   return (
-       <div className={styles.container}>
-        <h1>Hello Word Netx.js</h1>
-       </div>
+    <div className={styles.container}>
+      <ul>
+        <li>
+          <Link href="/products" legacyBehavior> 
+            <a>Produtos</a>
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/about" legacyBehavior> 
+            <a>Sobre nós</a>
+          </Link>
+        </li>
+
+      </ul>
+      
+      <h1>Hello Word Next.js</h1>
+    </div>
   )
 }
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,4 +1,15 @@
+import Link from "next/link"
+
+
 export default function Products() {
-    return <h1>Página de produtos</h1>
+    return (
+    <>
+        <h1>Página de produtos</h1>
+        <Link href="/" legacyBehavior>
+            <a>Voltar</a>
+        </Link>
+    </>
+
+    )
 }
 
