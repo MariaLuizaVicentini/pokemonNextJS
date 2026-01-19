4492838533133ded5cbb7bac7b3dbe4d6746a01b
malu vicentinim <vicentinimalu1@gmail.com>
2026-01-18 22:04:49 -0300

    feat

--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,7 @@ export default function Contact() {
     return (
         <>
             <Head>
-                <title>Páigna de Contato</title>
+                <title>Página de Contato</title>
             </Head>
             <h1>Página de Contato</h1>
         </>
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,8 +1,12 @@
 import Link from "next/link"
+import Head  from "next/head"
 
 export default function Products() {
     return (
     <>
+        <Head>
+            <title>Página de Produtos</title>
+        </Head>
         <h1>Página de produtos</h1>
         <Link href="/" legacyBehavior>
             <a>Voltar</a>
