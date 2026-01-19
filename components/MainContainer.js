05837ae3aef5ca8532b17ca9bf4da2a8a0d01ae5
malu vicentinim <vicentinimalu1@gmail.com>
2026-01-18 21:01:47 -0300

    feat: add novo componente de rodape

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -1,10 +1,13 @@
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 export default function MainContainer({children}) {
     return (
         <>
             <NavBar></NavBar>
             <div>{children}</div>
+            <Footer></Footer>
         </>
     )
 }
+
